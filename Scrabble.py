549b56aaf547daf001115d8d6e3c46ebce1a8cf1
Sean Mc <20smcnamara@psharvard.org>
2018-12-17 13:17:46 -0500

    Boops

--- Scrabble.py
+++ Scrabble.py
@@ -7,29 +7,47 @@ display_hight = 600
 black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
+BONUS_STRINGS = ["", "2x\nWS", "2x\nLS", "3x\nWS", "3x\nLS"]
 
-
-class Bonus:
-
-    strs = ["", ]
-
-    def __init__(self, bonus):
-        self.bonus = bonus
-        self.str = strs[bonus]
+# The below board matches with a picture at
+# https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiukpWtt6ffAhXtlOAKHRfzAWkQjRx6BAgBEAU&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FScrabble&psig=AOvVaw20SSGA8_KK0jKP66a4O2It&ust=1545155611561682
+BOARD_TILE_BONUSES = [[3, 0, 0, 2, 0, 0, 0, 3, 0, 0, 0, 2, 0, 0, 3],
+                      [0, 1, 0, 0, 0, 4, 0, 0, 0, 4, 0, 0, 0, 1, 0],
+                      [0, 0, 1, 0, 0, 0, 2, 0, 2, 0, 0, 0, 1, 0, 0],
+                      [2, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 1, 0, 0, 2],
+                      [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                      [0, 4, 0, 0, 0, 4, 0, 0, 0, 4, 0, 0, 0, 4, 0],
+                      [3, 0, 0, 1, 0, 0, 0, 5, 0, 0, 0, 1, 0, 0, 3],
+                      [0, 4, 0, 0, 0, 4, 0, 0, 0, 4, 0, 0, 0, 4, 0],
+                      [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                      [2, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 1, 0, 0, 2],
+                      [0, 0, 1, 0, 0, 0, 2, 0, 2, 0, 0, 0, 1, 0, 0],
+                      [0, 1, 0, 0, 0, 4, 0, 0, 0, 4, 0, 0, 0, 1, 0],
+                      [3, 0, 0, 2, 0, 0, 0, 3, 0, 0, 0, 2, 0, 0, 3]]
 
 
 class Tile:
 
-    BONUSES = []
-
     def __init__(self, cords, bonus=0):
         self.cords = cords
         self.bonus = bonus
+        self.str = BONUS_STRINGS[bonus]
 
+    def to_string(self):
+        return self.str
+
+    def place_letter(self, letter):
+        self.str = letter
+        return self.bonus
 
 
 class Board:
 
     def __init(self):
         self.dick = "cheese"
-        self.board = [[]]
+        self.board = []
+        for col in range(10):
+            self.board.append([])
+            for row in range(10):
+                self.board.append([])
+                self.board[row][col] = Tile([row, col], BOARD_TILE_BONUSES[row, col])
