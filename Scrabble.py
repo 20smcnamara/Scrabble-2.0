07eb5e9c2966fc605e09906aabd1d6e515dac844
Sean Mc <20smcnamara@psharvard.org>
2018-12-19 10:58:22 -0500

    Big update

--- Scrabble.py
+++ Scrabble.py
@@ -1,21 +1,19 @@
 import pygame
 
+
 def read_words():
     to_return = []
-    try:
-        file_object = open("words", "r")
-        string = file_object.read()
-        for word in string.split("\n"):
-            to_return.append(word)
-    except:
-        print(":(")
+    file_object = open("words", "r")
+    string = file_object.read()
+    for word in string.split("\n"):
+        to_return.append(word)
     return to_return
 
 
 pygame.init()
 
 display_width = 800
-display_hight = 600
+display_height = 600
 
 black = (0, 0, 0)
 white = (255, 255, 255)
